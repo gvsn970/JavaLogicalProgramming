4223699d4c6c9481750a4027b0d08cffab75c084
surya <surya.gosala@nexiilabs.com>
2019-02-25 15:10:44 +0530

    Added Fith Commit

--- java-read-write-excel-file-using-apache-poi-master/src/main/java/com/nexiilabs/sample/MissNumberRandom.java
+++ java-read-write-excel-file-using-apache-poi-master/src/main/java/com/nexiilabs/sample/MissNumberRandom.java
@@ -3,16 +3,20 @@ package com.nexiilabs.sample;
 public class MissNumberRandom {
 
 	public static void main(String[] args) {
-		int a[] = { 88, 89, 90, 92 };
+		int a[] = { 1,2,3,5,6 };
 		int len = a.length;
 		int sum = 0;
-		for (int i = 0; i < len; i++) {
-			for (int j = i; j < len; j++) {
-					if(a[i]+1 == a[j] ) {
-						System.out.println(a[i]);
-					}
+		int temp=0;
+		for (int i = 0; i <len; i++) {
+				temp=a[i]-a[i+1];
+			
+			if(temp == -2) {
+				
+				System.out.println(a[i]+1);
+				break;
+				
 			}
-
+		
 		}
 
 	}
