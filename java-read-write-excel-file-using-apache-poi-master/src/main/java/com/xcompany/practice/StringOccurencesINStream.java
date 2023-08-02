4f6c91df597ef996339d184580b453df3e08605f
gvsn970 <32327709+gvsn970@users.noreply.github.com>
2023-08-02 10:19:24 +0530

    added

--- java-read-write-excel-file-using-apache-poi-master/src/main/java/com/xcompany/practice/StringOccurencesINStream.java
+++ java-read-write-excel-file-using-apache-poi-master/src/main/java/com/xcompany/practice/StringOccurencesINStream.java
@@ -1,5 +1,7 @@
 package com.xcompany.practice;
 
+import java.util.HashSet;
+
 public class StringOccurencesINStream {
 
 	public static void main(String[] args) {
@@ -9,5 +11,20 @@ public class StringOccurencesINStream {
         String str = "a";
         System.out.println(a +b);
         System.out.println(str + b);
+        
+        
+        String[] s = {"abc", "ab", "cd", "ab", "abc"};
+//		Arrays.stream(s)
+//        .distinct()
+//        .forEach(System.out::println);
+		
+		 HashSet<String> uniqueElements = new HashSet<>();
+	        for (String element : s) {
+	            if (uniqueElements.add(element)) {
+	                System.out.println(element);
+	            }
+	        }
 		}
+	
+	
 }
